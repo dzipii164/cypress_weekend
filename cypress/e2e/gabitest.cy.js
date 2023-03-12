e2d5c71fdc53ca8ed2ac7d309b39f77a2522a216
Gabriela Pilatova <pilatova.gabriela164@gmail.com>
2023-03-12 08:52:18 +0100

    init cypress

---
+++ cypress/e2e/gabitest.cy.js
@@ -0,0 +1,15 @@
+
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+  })
+})
