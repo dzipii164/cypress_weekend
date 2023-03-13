7448bb93f617f9504403d785a0d6dbf2685829b4
Gabriela Pilatova <pilatova.gabriela@seznam.cz>
2023-03-13 21:55:40 +0100

    adjustments

--- cypress/e2e/cypress_weekend_task/cypress_weekend_task.cy.js
+++ cypress/e2e/cypress_weekend_task/cypress_weekend_task.cy.js
@@ -1,5 +1,5 @@
 
-describe('My First Test', () => {
+describe('cypress weekend entry task', () => {
   beforeEach(() => {
     cy.visit('https://www.kiwi.com/en/airport/bcn/barcelona-el-prat-barcelona-spain/')
     cy.clearCookies()
@@ -9,7 +9,7 @@ describe('My First Test', () => {
 
   it('Visits Barcelona airport', () => {
 
-//be.visible
+//be.visible, search form, h1
     cy.get('#sticky-search-form').should('be.visible')
     cy.get('[data-test="TrendingDestinations"]').should('be.visible')
     cy.get('[data-test="PopularFlights"]').should('be.visible')
@@ -17,15 +17,11 @@ describe('My First Test', () => {
     cy.get('[data-test="Faq"]').should('be.visible')
     cy.get('[data-test="TopAirlines"]').should('be.visible')
     cy.get('[data-test="NavBar"]').should('be.visible')
-  })
-
-  //search form and h1
-  it('Barcelona BCN', () => {
     cy.get('[data-test="SearchFieldItem-origin"]').should('contain', 'Barcelona BCN')
     cy.get('h1').should('contain', 'Barcelona–El Prat (BCN)')
   })
 
-  it('Ibiza trip', () => {
+  it.only('Ibiza trip', () => {
     
     //cy.get('[data-test="PictureCard"]').then(($cards) => {
      // const firstCard = $cards[0] // Cypress._.sample($cards)
@@ -50,5 +46,11 @@ describe('My First Test', () => {
     cy.intercept('https://api.skypicker.com/umbrella/v2/graphql?featureName=SearchReturnItinerariesQuery').as('search')
     cy.wait('@search')
    // cy.get('[data-test="ResultList"]')
+
+   //reservation
+   cy.get('[data-test="ResultCardWrapper"]').find('[data-test="BookingButton"]').first().click()
+   cy.get('[data-test="MagicLogin-RequiredLogin"]')
+   cy.get('[data-test="MagicLogin-GuestTextLink"]').click()
+   cy.url().should('include', 'booking')
   })
 })
