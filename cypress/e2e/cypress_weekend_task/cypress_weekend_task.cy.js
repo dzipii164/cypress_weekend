1883b0735512e297d078bbdfeaa7545cb5623dc7
Gabriela Pilatova <pilatova.gabriela@seznam.cz>
2023-03-15 12:55:33 +0100

    repeat test 10 times

--- cypress/e2e/cypress_weekend_task/cypress_weekend_task.cy.js
+++ cypress/e2e/cypress_weekend_task/cypress_weekend_task.cy.js
@@ -1,56 +1,58 @@
 
-describe('cypress weekend entry task', () => {
-  beforeEach(() => {
-    cy.visit('https://www.kiwi.com/en/airport/bcn/barcelona-el-prat-barcelona-spain/')
-    cy.clearCookies()
+Cypress._.times(10, () => {
+  describe('cypress weekend entry task', () => {
+    beforeEach(() => {
+      cy.visit('https://www.kiwi.com/en/airport/bcn/barcelona-el-prat-barcelona-spain/')
+      cy.clearCookies()
 
-    cy.get('[data-test="CookiesPopup-Accept"]').click() // cookies
-  })
+      cy.get('[data-test="CookiesPopup-Accept"]').click() // cookies
+    })
 
-  it('Visits Barcelona airport', () => {
+    it('Visits Barcelona airport', () => {
 
-//be.visible, search form, h1
-    cy.get('#sticky-search-form').should('be.visible')
-    cy.get('[data-test="TrendingDestinations"]').should('be.visible')
-    cy.get('[data-test="PopularFlights"]').should('be.visible')
-    cy.get('[data-test="DestinationsMap"]').should('be.visible')
-    cy.get('[data-test="Faq"]').should('be.visible')
-    cy.get('[data-test="TopAirlines"]').should('be.visible')
-    cy.get('[data-test="NavBar"]').should('be.visible')
-    cy.get('[data-test="SearchFieldItem-origin"]').should('contain', 'Barcelona BCN')
-    cy.get('h1').should('contain', 'Barcelona–El Prat (BCN)')
-  })
+      //be.visible, search form, h1
+      cy.get('#sticky-search-form').should('be.visible')
+      cy.get('[data-test="TrendingDestinations"]').should('be.visible')
+      cy.get('[data-test="PopularFlights"]').should('be.visible')
+      cy.get('[data-test="DestinationsMap"]').should('be.visible')
+      cy.get('[data-test="Faq"]').should('be.visible')
+      cy.get('[data-test="TopAirlines"]').should('be.visible')
+      cy.get('[data-test="NavBar"]').should('be.visible')
+      cy.get('[data-test="SearchFieldItem-origin"]').should('contain', 'Barcelona BCN')
+      cy.get('h1').should('contain', 'Barcelona–El Prat (BCN)')
+    })
 
-  it.only('Ibiza trip', () => {
-    
-    //cy.get('[data-test="PictureCard"]').then(($cards) => {
-     // const firstCard = $cards[0] // Cypress._.sample($cards)
-     // firstCard.click()
+    it.only('Ibiza trip', () => {
+
+      //cy.get('[data-test="PictureCard"]').then(($cards) => {
+      // const firstCard = $cards[0] // Cypress._.sample($cards)
+      // firstCard.click()
 
       // test that current url matches what we clicked
-     // const cardHref = Cypress.$(firstCard).attr("href")
-     // cy.url().should('include', cardHref)
-    // })
+      // const cardHref = Cypress.$(firstCard).attr("href")
+      // cy.url().should('include', cardHref)
+      // })
 
-    // cy.get('[data-test="PlacePickerInput-destination"] > [data-test="PlacePickerInputPlace"]').then(($searchButton) => {
-    //   cy.url().should('include', $searchButton.text().toLowerCase())
-    // })
+      // cy.get('[data-test="PlacePickerInput-destination"] > [data-test="PlacePickerInputPlace"]').then(($searchButton) => {
+      //   cy.url().should('include', $searchButton.text().toLowerCase())
+      // })
 
-    cy.get('[data-test="PictureCard"]').first().click()
-    cy.url().should('include', 'search/results')
-    
-  //one cabin bag
-    cy.get('[data-test="FilterHeader-bags"]').find('[aria-label="increment"]').first().click()
+      cy.get('[data-test="PictureCard"]').first().click()
+      cy.url().should('include', 'search/results')
 
-  //results updated
-    cy.intercept('https://api.skypicker.com/umbrella/v2/graphql?featureName=SearchReturnItinerariesQuery').as('search')
-    cy.wait('@search')
-   // cy.get('[data-test="ResultList"]')
-
-   //reservation
-   cy.get('[data-test="ResultCardWrapper"]').find('[data-test="BookingButton"]').first().click()
-   cy.get('[data-test="MagicLogin-RequiredLogin"]')
-   cy.get('[data-test="MagicLogin-GuestTextLink"]').click()
-   cy.url().should('include', 'booking')
+      //one cabin bag
+      cy.get('[data-test="FilterHeader-bags"]').find('[aria-label="increment"]').first().click()
+      cy.url().should('include', 'bags=1.0-')
+      
+      //results updated 
+      cy.intercept('https://api.skypicker.com/umbrella/v2/graphql?featureName=SearchReturnItinerariesQuery').as('search')
+      cy.wait('@search')
+      
+      //reservation
+      cy.get('[data-test="ResultCardWrapper"]').find('[data-test="BookingButton"]').first().click()
+      cy.get('[data-test="MagicLogin-RequiredLogin"]')
+      cy.get('[data-test="MagicLogin-GuestTextLink"]').click()
+      cy.url().should('include', 'booking')
+    })
   })
 })
